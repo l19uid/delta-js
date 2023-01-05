00d518aa214effb9870ef35574a1af3780453d9a
Tomáš Vik <GepardCZE@post.cz>
2023-01-05 08:38:20 +0100

    metroboomin :fire: :explosion: :microphone:

--- nvm-test/index.js
+++
@@ -1,9 +0,0 @@
-import chalk from 'chalk';
-import Example from './Example.js';
-
-const log = console.log;
-
-log(chalk.blue('Hello world'));
-
-const example = new Example(10,10);
-log(chalk.hex('#231231').bold(example.sum()));
